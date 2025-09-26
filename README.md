8c960ccbd9d3e82dbca8054d0984a6ad30455d4e
daveclinton <clintondavid46@gmail.com>
2025-09-26 21:49:17 +0300

    feat: add read me

---
+++ README.md
@@ -0,0 +1,3 @@
+// const files = await ittybit.files.list();
+
+// console.log("List of Files", files);
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,16 @@ import { IttybitClient } from "@ittybit/sdk";
 
 console.log("Testing Ittybit SDK...");
 
-const ittybit = new IttybitClient({ apiKey: "ITTYBIT_API_KEY" });
+const ittybit = new IttybitClient({
+  apiKey: process.env.ITTYBIT_API_KEY!,
+});
+
+const task = await ittybit.tasks.create({
+  kind: "ingest",
+  url: "https://eorkikxtbgamoosfudlt.supabase.co/storage/v1/object/public/ittybit-storage/Tutorial%20Thumbnails%2011.png",
+  description: "Thumbnail Image",
+  webhook_url: "https://webhook.site/982e58ce-48c7-4ad4-a11d-bcf27e6b76c7",
+});
+
+console.log("Task created:", task.id);
+console.log("Task status:", task.status);
